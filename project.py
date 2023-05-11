5a763b54752dd71f418b01404fb315defb20daaf
xav <XAVIERLKW.22@ichat.sp.edu.sg>
2023-05-11 19:44:14 +0800

    imported some files

---
+++ project.py
@@ -0,0 +1,15 @@
+'''import stuff, idk how many lines we need'''
+import webbrowser  #this lets you open websites idk how it'll help but yea
+import qrcode
+from qrcode.image.pure import PyPNGImage
+
+
+
+
+
+
+
+
+
+
+
